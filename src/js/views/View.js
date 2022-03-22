c69f4dd880eabf46edce14072399b7ac827a1b9a
Theodoric-Graham <theodoricgraham@hotmail.com>
2022-03-22 13:52:18 -0500

    added comments for clarity

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -15,17 +15,24 @@ export default class View {
   update(data) {
     this._data = data;
     const newMarkup = this._generateMarkup();
-
+    //converts the string into real DOM node objects, basically creates a virtual DOM
     const newDom = document.createRange().createContextualFragment(newMarkup);
+    console.log(newDom);
     const newElements = Array.from(newDom.querySelectorAll('*'));
     const curElements = Array.from(this._parentElement.querySelectorAll('*'));
     // console.log(curElements);
+    //we will see all the elements that will be contained in this newDom
+    //element, that we created from generating the markup on the updated data.
+    //produces a node list, that we are turning into an array using Array.from
     // console.log(newElements);
 
     newElements.forEach((newEl, i) => {
       const curEl = curElements[i];
+      //.isEqualNode will compare the contents of one node to the one we pass in
       console.log(curEl, newEl.isEqualNode(curEl));
-      // Updates changed TEXT
+      // Updates changed TEXT - the value of nodeValue will be null for most node
+      //types, but if its text you get the content of the text node
+      //so if none of the nodeValues contain text directly, it will be false
       if (
         !newEl.isEqualNode(curEl) &&
         newEl.firstChild?.nodeValue.trim() !== ''
