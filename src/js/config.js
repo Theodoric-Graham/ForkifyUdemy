e3c9e2f4e2ea87033eae703d93984211453938c9
Theodoric-Graham <theodoricgraham@hotmail.com>
2022-04-06 17:15:25 -0500

    testing with new key, commented old key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,6 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '6846aa8a-0fc2-4427-9e95-b5674d1bb940';
+export const KEY = 'b4ac36a9-1588-4216-b94c-a4c25741ef0d';
+// 6846aa8a-0fc2-4427-9e95-b5674d1bb940
 export const MODAL_CLOSE_SEC = 2.5;
